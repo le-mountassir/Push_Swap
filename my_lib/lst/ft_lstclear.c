99461ccc6f106d7a088c503da5319765395140b6
mountassir-007 <ahmedelmountassir10@gmail.com>
2021-12-24 12:13:12 +0100

    clear list

---
+++ my_lib/lst/ft_lstclear.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahel-mou <ahel-mou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/09 18:19:38 by ahel-mou          #+#    #+#             */
+/*   Updated: 2021/12/23 11:09:07 by ahel-mou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../mand_lib.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*tmp;
+	t_list	*next;
+
+	tmp = *lst;
+	while (tmp)
+	{
+		next = tmp->next;
+		ft_lstdelone(tmp, del);
+		tmp = next;
+	}
+	*lst = NULL;
+}
